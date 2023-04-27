90c59c99f096a6bbd6f74ee936e35be600eb7fd8
Samuel Reuben <sreuben1998@gmail.com>
2023-04-27 18:21:34 +0000

    Almost done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,8 +1,26 @@
 class DirectorsController < ApplicationController
 
   def index
+    @list_of_directors = Director.all
 
     render({ :template => "directors_templates/index.html.erb"})
   end
 
+  def wisest
+
+    @oldest = Director.where.not({ :dob => nil}).order({ :dob => :asc }).at(0)
+    
+    render({ :template => "directors_templates/eldest.html.erb"})
+  end
+
+  def director_details
+    # params look like {"an_id"=>"42"}
+
+    the_id = params.fetch("an_id")
+
+    @the_director = Director.where({ :id => the_id }).at(0)
+
+    render({ :template => "directors_templates/show.html.erb"})
+
+  end
 end
