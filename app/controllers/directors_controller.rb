51a3b67bb1bb9df6103a35008a0cda713fef97dd
Samuel Reuben <sreuben1998@gmail.com>
2023-05-02 15:07:21 +0000

    Added show details link to directors show

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -15,7 +15,6 @@ class DirectorsController < ApplicationController
 
   def director_details
     # params look like {"an_id"=>"42"}
-
     the_id = params.fetch("an_id")
 
     @the_director = Director.where({ :id => the_id }).at(0)
